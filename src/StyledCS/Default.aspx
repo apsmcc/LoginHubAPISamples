c1cd7c3dc741ac944bf9c6f8964db3662ced5480
Cam Birch <cam@maintenanceconnection.ca>
2016-08-22 08:31:29 -0600

    Add a CSS styled sample.

---
+++ src/StyledCS/Default.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Async="true" Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>MC LoginHub&trade; API</title>
+	<link rel="stylesheet" href="default.css" type="text/css" media="screen" />
+</head>
+<body>
+	<main>
+		<header>
+			<div class="mc-logo" alt="Web-Based CMMS Maintenance Management Software"></div>
+			<div class="logo-rule-spacer">
+				<div class="logo-rule"></div>
+			</div>
+			<h1>Login Hub&trade;</h1>
+		</header>
+
+		<div class="horizontal-line"></div>
+
+		<div class="main-contents">
+			<div class="contents">
+
+<section id="loginForm" class="login-form">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-small-5 col-max-form-6 hidden-small">
+				<div class="login-mc-image"></div>
+			</div>
+			<div class="col-small-7 col-max-form-6 login-mc-form">
+				<h3 class="welcome">Welcome.</h3>
+				<p>Please enter your Member ID and Password.</p>
+
+				<form method="post" role="form" class="form" runat="server">
+					<label for="Username">Member ID:</label>
+					<asp:TextBox CssClass="form-control" ID="Username" placeholder="Member ID" runat="server" />
+
+					<label for="Password">Password:</label>
+					<asp:TextBox CssClass="form-control" ID="Password" TextMode="Password" placeholder="Password" runat="server" />
+
+					<p class="text-danger">
+						<asp:Literal ID="ErrorMessage" runat="server"></asp:Literal>
+					</p>
+
+		            <asp:Button CssClass="btn btn-default" ID="Submit" runat="server" Text="Log in" />
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+			</div>
+		</div>
+
+		<div class="horizontal-line"></div>
+
+		<footer>
+			<div class="copyright-details">
+				<div class="copyright-mc">Copyright © 2002-2015 Maintenance Connection, Inc. All Rights Reserved.</div>
+				<div class="copyright-disclaimer">
+					By logging in, you agree to the Terms of Use, including the electronic delivery of important disclosures and other information
+					contained in the Agreement. Please read the
+					<a href="#">Terms of Use</a> and
+					<a href="#">Privacy Policy</a> before you log in.
+				</div>
+				<div class="copyright-mcc">
+					<a href="http://www.maintenanceconnection.ca">Copyright © 2006-2016 Maintenance Connection Canada, Inc. All Rights Reserved.</a>
+				</div>
+			</div>
+		</footer>
+	</main>
+</body>
+</html>
---
+++ src/StyledCS/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using LoginHub.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+	protected override void OnInit(EventArgs e)
+	{
+		base.OnInit(e);
+		LoginApiMethods.BaseUrl = "http://localhost:64406/";
+		LoginApiMethods.AuthenticationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjFrdUptVkN3VlN2VTUzMGNpSEp0bmhUWldObyJ9.eyJhdWRpZW5jZSI6ImY5MDljMGYwLWYwNmMtNGNhYy1hZWI4LTZkOTVmYTA5YjBlMCIsImFwcGxpY2F0aW9uIjoiTG9naW5IdWIgQVBJIiwidXNlciI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImlwQWRkcmVzcyI6Ijo6MSIsImlzcyI6IkxvZ2luSHViIEFQSSIsImF1ZCI6ImY5MDljMGYwLWYwNmMtNGNhYy1hZWI4LTZkOTVmYTA5YjBlMCIsIm5iZiI6MTQ3MDc1OTk4Mn0.NM4PMz_9dtFrfwxQ4oV7IRcSRNNvlrLhlZb6aTYFTci6hJCAMIkIsnbZrWx9etTM32pBdTQRO7IrIi5RbUmyb1orxb2-2tt3Dzc4GoroxW7MNNXeVrT-aDXrKbNUbS6gCUfAL8nqVEOROfe7tFfF0nFGxZmRmOhUudV9jX1Vxl0GGZpsILyLvkpzmqAJqqGnnmDk6nHR8-RlGXhXhVTP7t9DwT0EVvyk4ozhp1BsYfraXTzLdlYUuALtyNdvYcD8v_bJqKi6vsWf8VSkmsIeSYCG3PPohYrRiG6J7aFddSfdqzLkMsPv_qRQRcWfj2RTO1SwqOpYZ-w80uaYMc1OG5roKWBr_hQ1261rixXaiOlj3rIaV-KyESqYJaaOPT63Qh8i5d75WSkBWdaLn598GCkisEmkw2GD8bE5RiphcXBHzBjOiVfYYPfig1H6RQfCigsnOFE3_HqLU6DtLAKVoYrd_WevRXO_VNpoBHpIJR_BXilSQAvu9VYN08kkkps1PzoFhISpTfIgoIWKZd4dw2-LlcDALPFXVTqK6SfU-vPkCp2PTGBj5Pb_jA9sSObvoKNaqcGM-om1IZQbUXlTZcf8fl2ndf-0SoLtWKB1ev6aR81NZHBijeh01f1mDiu6G8nkxJCazhGqgIK0m_EpPR5TVIaZn_QHaGiEvt-Wv28";
+
+		// Enable the simulator when
+		if (HttpContext.Current.IsDebuggingEnabled)
+		{
+			LoginApiMethods.IsSimulationMode = true;
+			LoginApiMethods.SimulationModeType = SimulationModeType.RandomlySimulateSomething;
+		}
+
+		Submit.Click += Submit_Click;
+	}
+
+	private async void Submit_Click(object sender, EventArgs e)
+	{
+		// Clear previous errors
+		ErrorMessage.Text = "";
+
+		// Attempt to log the user in
+		LoginTokenInfo loginToken;
+		try
+		{
+			loginToken = await LoginApiMethods.CreateLoginToken(Username.Text, Password.Text);
+		}
+		catch (Exception ex)
+		{
+			ErrorMessage.Text = "There was a communication error with the server, please try again later or contact IT support.";
+			return;
+		}
+
+		// Show an error if the user/password combo was invalid
+		if (loginToken == null || loginToken.IsSuccessful == false)
+		{
+			ErrorMessage.Text = "Username or password was incorrect, please check and try again.";
+			return;
+		}
+
+		// Send the user into the LoginHub
+		Response.Redirect(LoginApiMethods.CreateLoginUrlForToken(loginToken), false);	//NOTE: the last value of false is required or ASP.NET will throw an error
+	}
+
+}
